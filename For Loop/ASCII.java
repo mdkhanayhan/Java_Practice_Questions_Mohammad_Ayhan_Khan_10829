93d56451c8dc35939dd83231c9a17f72a38d57e1
mdkhanayhan <mdkhanayhan3@gmail.com>
2026-03-02 10:31:38 +0530

    Create ASCII.java

---
+++ For Loop/ASCII.java
@@ -0,0 +1,21 @@
+public class ASCII {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.println(c + " = " + (int) c);
+        }
+    }
+}
+class ASCII2 {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.println(c + " = " + (int) c);
+        }
+    }
+}
+class ASCII3 {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.println(c + " = " + (int) c);
+        }
+    }
+}
